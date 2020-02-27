cd0a6e090a34a8224d289641c48199e1c620e661
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    sm6250-common: Panic to recovery not bootloader
    Change-Id: Iaf737890e3c860df504629d35a4a68c494f39910

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,6 +113,7 @@ TARGET_KERNEL_CLANG_VERSION := r498229
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
