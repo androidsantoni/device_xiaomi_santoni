994eef2cad7669d75c5cddb28be45f8dce5c8009
Alexander Winkowski <dereference23@outlook.com>
2023-02-01 15:38:09 +0800

    sm6250-common: Switch to prebuilt audio HAL
    * Add usb_audio_policy_configuration.xml
    * Build libtinycompress
    * Redo the audio props from stock
    
    Change-Id: I27315f8a167e1d6dc223df1f41c9dcea5b391b05

--- common.mk
+++ common.mk
@@ -47,14 +47,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    audio.primary.atoll \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler \
     libtinycompress
 
 PRODUCT_PACKAGES += \
-    liba2dpoffload \
     libbatterylistener \
     libcirrusspkrprot \
     libcomprcapture \
@@ -73,10 +71,10 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml 
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
