cddeb7812983685c07dc134986fb91db29afae44
Omansh Krishn <omansh11597@gmail.com>
2023-11-08 06:07:18 +0530

    santoni: EvolutionX bringup
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -568,3 +568,10 @@ ART_BUILD_HOST_DEBUG := false
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Extra Apps
+PRODUCT_PACKAGES += \
+    CalculatorGooglePrebuilt \
+    NgaResources \
+    WallpaperEmojiPrebuilt \
+    WallpaperEffect
