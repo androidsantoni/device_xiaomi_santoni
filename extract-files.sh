LineageOS/android_device_xiaomi_sm6250-common

sm6250-common: Suppress CamX debug
Change-Id: I001be38a19664bcf4994fc88fa1f4448c8cc7891

extract-files.sh
@@ -60,6 +60,10 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/camera/camxoverridesettings.txt)
+            sed -i "s/0x10080/0/g" "${2}"
+            sed -i "s/0x1F/0x0/g" "${2}"
+            ;;
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;

androidsantoni/device_xiaomi_santoni

extract-files.sh
@@ -48,6 +48,10 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/camera/camxoverridesettings.txt)
+            sed -i "s/0x10082/0/g" "${2}"
+            sed -i "s/0x1F/0x0/g" "${2}"
+            ;;
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
             sed -i "s/4\.0/4\.1/g" "${2}"
         ;;