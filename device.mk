99de48050d5df4aebbc355da6dfbaab1192971bb
RisenID <risenid@duck.com>
2024-01-14 21:34:38 +0530

    santoni: Correct SystemUI package name
    Apk name and path was changed as seen here -> https://github.com/PixelExperience/vendor_pixel-framework/commit/8be4fc80c0af1a38386b7dc23b7392d0a9e64669
    
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -159,10 +159,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
-		android.hardware.bluetooth.a2dp@1.0.vendor \
-		android.hardware.bluetooth.audio@2.2-impl \
-		android.hardware.bluetooth.audio@2.1-impl \
-		android.hardware.bluetooth.audio@2.0-impl \
+	android.hardware.bluetooth.a2dp@1.0.vendor \
+	android.hardware.bluetooth.audio@2.2-impl \
+	android.hardware.bluetooth.audio@2.1-impl \
+	android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
     vendor.qti.hardware.btconfigstore@2.0.vendor:64 \
@@ -203,7 +203,7 @@ PRODUCT_COPY_FILES += \
 
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
+    SystemUIGoogle \
     Settings
 
 # Display
