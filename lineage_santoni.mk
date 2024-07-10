3f257a5669a60af348ea4c28ca92c60120551c36
Omansh Krishn <omansh@duck.com>
2024-07-10 15:52:21 +0530

    santoni: TARGET_CALL_RECORDING_SUPPORTED -> true
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -24,6 +24,7 @@ $(call inherit-product, device/xiaomi/santoni/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_CALL_RECORDING_SUPPORTED := true
 
 DERP_VERSION_APPEND_TIME_OF_DAY := true
 
