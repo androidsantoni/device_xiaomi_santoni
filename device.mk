93ff2b4f7973e38b57355a35617a9bbccd7fbf46
Michael Bestas <mikeioannina@gmail.com>
2024-07-13 17:55:58 +0530

    santoni: Build missing lib for a14 qpr3
    ERROR:
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "liblzma.so" not found: needed by main executable
    
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -269,6 +269,9 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.2.vendor \
     fakelogprint
 
+PRODUCT_PACKAGES += \
+    liblzma.vendor:64
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
