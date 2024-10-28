3e0ed05a09b39f20aeb95557f7e9d08698e4b368
Omansh Krishn <omansh@duck.com>
2024-10-28 12:26:07 +0530

    santoni: rootdir: Override CLO's `init.qcom.usb.rc`
    Signed-off-by: Omansh Krishn <omansh@duck.com>

--- device.mk
+++ device.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
+    santoni.init.qcom.usb.rc \
     init.msm.usb.configfs.rc \
     init.recovery.qcom.rc \
     init.target.rc \
