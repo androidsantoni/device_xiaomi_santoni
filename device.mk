e25f140d00b43ca88ac7e14679debd841e88f1dc
Omansh Krishn <omansh@duck.com>
2025-02-03 09:17:16 +0530

    santoni: Remove Bluetooth.a2dp@1 HIDL HAL
    Support for the HAL android.hardware.bluetooth.a2dp@1.0 has been
    removed by Ie0c624ca8e917af8e06208871a5353382fe3aada
    
    Signed-off-by: Omansh Krishn <omansh@duck.com>

--- device.mk
+++ device.mk
@@ -166,8 +166,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.bluetooth.a2dp@1.0 \
-    android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
     vendor.qti.hardware.btconfigstore@2.0.vendor:64 \
