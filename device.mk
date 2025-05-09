f0f8f9e3f7097a31cdc28d4cbcc07281b4bb5b26
dread <dreadcreator5@gmail.com>
2025-05-09 19:31:36 +0530

    santoni: Build required Wi-fi Interfaces
    - without these interfaces, wifi hal dosen't seem to work
    
    FIXES:
    Failed to get chip capabilities from legacy HAL: NOT_SUPPORTED
    
    Signed-off-by: dread <dreadcreator5@gmail.com>
    Change-Id: I20990341f9d3614b1675d5565efe743f76a1718d
    Signed-off-by: Omansh Krishn <omansh@duck.com>

--- device.mk
+++ device.mk
@@ -553,7 +553,25 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-service
+    android.hardware.wifi-service \
+    android.hardware.wifi@1.1.vendor \
+    android.hardware.wifi@1.2.vendor \
+    android.hardware.wifi@1.3.vendor \
+    android.hardware.wifi@1.4.vendor \
+    android.hardware.wifi@1.5.vendor \
+    android.hardware.wifi@1.6.vendor \
+    android.hardware.wifi.hostapd@1.0.vendor \
+    android.hardware.wifi.hostapd@1.1.vendor \
+    android.hardware.wifi.hostapd@1.2.vendor \
+    android.hardware.wifi.hostapd@1.3.vendor \
+    android.hardware.wifi.supplicant@1.0.vendor \
+    android.hardware.wifi.supplicant@1.1.vendor \
+    android.hardware.wifi.supplicant@1.2.vendor \
+    android.hardware.wifi.supplicant@1.3.vendor \
+    android.hardware.wifi.supplicant@1.4.vendor \
+    android.hardware.tetheroffload.config@1.0.vendor \
+    android.hardware.tetheroffload.control@1.0.vendor \
+    android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
